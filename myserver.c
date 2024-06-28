199ba2dd69150ce02ddeba233cd99edf150f7972
root <root@localhost.localdomain>
2024-06-28 10:19:46 +0200

    Réagencement du code, séparation de la création de socket du main vers fonction tierce

--- myserver.c
+++ myserver.c
@@ -21,7 +21,7 @@ typedef struct {
     char sender[BUFFER_SIZE];
 } Message;
 
-// Structure pour une room
+// Structure pour une room Max 5 users
 typedef struct {
     int clients[MAX_USERS_PER_ROOM];
     int count;
@@ -29,15 +29,115 @@ typedef struct {
     int history_count;
 } Room;
 
-// Structure pour un client
+// Structure pour un client avec socket Id et pseudo
 typedef struct {
     int socket;
     char pseudo[BUFFER_SIZE];
 } Client;
 
+typedef struct {
+    char *sequence;
+    char *emoticon;
+} EmoticonMap;
+
+EmoticonMap emoticons[] = {
+    {"smile", "😊"},
+    {"laugh", "😂"},
+    {"wink", "😉"},
+    {"blush", "😊"},
+    {"heart-eyes", "😍"},
+    {"joy", "😅"},
+    {"yum", "😋"},
+    {"sunglasses", "😎"},
+    {"stuck-out-tongue-winking-eye", "😜"},
+    {"sleepy", "😪"},
+    {"joy-cat", "😹"},
+    {"kiss-heart", "😘"},
+    {"thumbs-up", "👍"},
+    {"thumbs-down", "👎"},
+    {"fist", "✊"},
+    {"raised-fist", "✊"},
+    {"wave", "👋"},
+    {"clap", "👏"},
+    {"handshake", "🤝"},
+    {"star", "⭐️"},
+    {"sun", "☀️"},
+    {"moon", "🌙"},
+    {"rainbow", "🌈"},
+    {"flower", "🌸"},
+    {"gift", "🎁"},
+    {"cake", "🍰"},
+    {"pizza", "🍕"},
+    {"hamburger", "🍔"},
+    {"fries", "🍟"},
+    {"ice-cream", "🍦"},
+    {"coffee", "☕️"},
+    {"beer", "🍺"},
+    {"wine", "🍷"},
+    {"cocktail", "🍹"},
+    {"bottle", "🍾"},
+    {"balloon", "🎈"},
+    {"music", "🎵"},
+    {"movie", "🎥"},
+    {"computer", "💻"},
+    {"phone", "📱"},
+    {"book", "📚"},
+    {"money", "💵"},
+    {"calendar", "📅"},
+    {"house", "🏠"},
+    {"car", "🚗"},
+    {"airplane", "✈️"},
+    {"boat", "⛵️"},
+    {"train", "🚂"},
+    {"happy", "😄"},
+    {"sad", "😞"},
+    {"angry", "😡"},
+    {"surprised", "😮"},
+    {"tired", "😴"},
+    {"sick", "🤒"},
+    {"crazy", "😜"},
+    {"scared", "😱"},
+    {"love", "😍"},
+    {"party", "🎉"},
+    {"work", "💼"},
+    {"study", "📖"},
+    {"sport", "⚽️"},
+    {"game", "🎮"},
+    {"holiday", "🏖️"},
+    {"health", "🏥"},
+    {"school", "🏫"},
+    {"euro", "💶"},
+    {"dollar", "💵"},
+    {"pound", "💷"},
+    {"roll", "🍣"},
+    {"sushi", "🍱"},
+    {"rice-ball", "🍙"},
+    {"pine-decoration", "🎍"},
+    {"drink", "🍸"},
+    {"strawberry", "🍓"},
+    {"lemon", "🍋"},
+    {"peach", "🍑"},
+    {"cherries", "🍒"},
+    {"chestnut", "🌰"},
+};
+
+#define EMOTICON_COUNT (sizeof(emoticons) / sizeof(EmoticonMap))
 // Déclarations globales
 sem_t mutex;
 Room rooms[MAX_ROOMS];
+
+// Fonction pour convertir le texte en émoticônes
+void convert_text_to_emoticons(char *buffer) {
+    for (int i = 0; i < EMOTICON_COUNT; ++i) {
+        char *pos;
+        while ((pos = strstr(buffer, emoticons[i].sequence)) != NULL) {
+            size_t len_sequence = strlen(emoticons[i].sequence);
+            size_t len_emoticon = strlen(emoticons[i].emoticon);
+            memmove(pos + len_emoticon, pos + len_sequence, strlen(pos + len_sequence) + 1);
+            memcpy(pos, emoticons[i].emoticon, len_emoticon);
+        }
+    }
+}
 
 // Gestion des signaux
 void sigIntHandler(int sig) {
@@ -58,6 +158,42 @@ void exitFunction() {
     close(server_socket);
 }
 
+// Fonction pour initialiser l'adresse
+void initAdresse(struct sockaddr_in *adresse) {
+    adresse->sin_family = AF_INET;
+    adresse->sin_addr.s_addr = INADDR_ANY;
+    adresse->sin_port = htons(PORT);
+}
+
+// Fonction pour initialiser le socket
+int initSocket(struct sockaddr_in *adresse) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("Could not create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
+        perror("Échec de l'activation de SO_REUSEADDR");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(sock, (struct sockaddr*)adresse, sizeof(*adresse)) < 0) {
+        perror("Bind failed");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sock, 3) < 0) {
+        perror("Listen failed");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    return sock;
+}
 
 // Fonction pour gérer les clients une fois connecté au serveur 
 void* handle_client(void* client_socket) {
@@ -65,7 +201,7 @@ void* handle_client(void* client_socket) {
     
     client.socket = *(int*)client_socket;
     char buffer[BUFFER_SIZE];
-    int n;
+    int n = 0 ;
     int room_id = -1;
 
     // Demander le pseudo au client
@@ -91,7 +227,6 @@ void* handle_client(void* client_socket) {
         sprintf(buffer, "%s: %s", rooms[room_id].history[i].sender, rooms[room_id].history[i].message);
         write(client.socket, buffer, strlen(buffer));
     }
-
     /*Dans le serveur de chat, certaines données partagées entre les threads doivent être protégées contre l'accès concurrent.La liste des clients dans une salle de discussion (rooms[i].clients) ou l'historique des messages (rooms[i].history). Utiliser un mutex permet de synchroniser l'accès à ces données critiques afin d'éviter les conflits lorsque plusieurs threads tentent d'accéder ou de modifier ces données en même temps.*/
     /*Pour éviter que deux threads essayent de modifier les structures de données simultanément, on utilise le mutex pour bloquer */
     sem_post(&mutex);
@@ -99,39 +234,42 @@ void* handle_client(void* client_socket) {
     sprintf(buffer, "Vous êtes dans la room %d\n", room_id);
     write(client.socket, buffer, strlen(buffer));
 
-while ((n = read(client.socket, buffer, sizeof(buffer) - 1)) > 0) {
-    buffer[n] = '\0';
-    printf("Client %s (room %d): %s\n", client.pseudo, room_id, buffer);
-
-    // Diffuser le message à tous les autres clients de la même room
-    sem_wait(&mutex);
-    // Ajouter le message à l'historique de la room
-    if (rooms[room_id].history_count < HISTORY_SIZE) {
-        strcpy(rooms[room_id].history[rooms[room_id].history_count].message, buffer);
-        strcpy(rooms[room_id].history[rooms[room_id].history_count].sender, client.pseudo);
-        rooms[room_id].history_count++;
-    } else {
-        // Décaler les messages dans l'historique circulaire
-        for (int i = 0; i < HISTORY_SIZE - 1; ++i) {
-            strcpy(rooms[room_id].history[i].message, rooms[room_id].history[i + 1].message);
-            strcpy(rooms[room_id].history[i].sender, rooms[room_id].history[i + 1].sender);
+    while ((n = read(client.socket, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[n] = '\0';
+        printf("Client %s (room %d): %s\n", client.pseudo, room_id, buffer);
+        // Convertir le texte en émoticônes avant de diffuser le message
+        convert_text_to_emoticons(buffer);
+        // Diffuser le message à tous les autres clients de la même room
+        sem_wait(&mutex);
+        // Ajouter le message à l'historique de la room
+        if (rooms[room_id].history_count < HISTORY_SIZE) {
+            strcpy(rooms[room_id].history[rooms[room_id].history_count].message, buffer);
+            strcpy(rooms[room_id].history[rooms[room_id].history_count].sender, client.pseudo);
+            rooms[room_id].history_count++;
+        } else {
+            for (int i = 0; i < HISTORY_SIZE - 1; ++i) {
+                strcpy(rooms[room_id].history[i].message, rooms[room_id].history[i + 1].message);
+                strcpy(rooms[room_id].history[i].sender, rooms[room_id].history[i + 1].sender);
+            }
+            strcpy(rooms[room_id].history[HISTORY_SIZE - 1].message, buffer);
+            strcpy(rooms[room_id].history[HISTORY_SIZE - 1].sender, client.pseudo);
         }
-        strcpy(rooms[room_id].history[HISTORY_SIZE - 1].message, buffer);
-        strcpy(rooms[room_id].history[HISTORY_SIZE - 1].sender, client.pseudo);
-    }
-    
-    // Préparer le message à diffuser aux autres clients
-    char message_to_send[BUFFER_SIZE];
-    sprintf(message_to_send, "%s->%s", client.pseudo, buffer);
+        // Préparer le message à diffuser aux autres clients
+        char message_to_send[BUFFER_SIZE];
+        sprintf(message_to_send, "%s->%s", client.pseudo, buffer);
 
-    for (int i = 0; i < rooms[room_id].count; i++) {
-        if (rooms[room_id].clients[i] != client.socket) {
-            write(rooms[room_id].clients[i], message_to_send, strlen(message_to_send));
+        for (int i = 0; i < rooms[room_id].count; ++i) {
+            if (rooms[room_id].clients[i] != client.socket) {
+                sprintf(buffer, "%s: %s\n", client.pseudo, msg.message);
+                write(rooms[room_id].clients[i], buffer, strlen(buffer));
+            } else {
+                sprintf(buffer, "Moi: %s\n", msg.message);
+                write(client.socket, buffer, strlen(buffer));
+            }
         }
-    }
 
-    sem_post(&mutex);
-}
+        sem_post(&mutex);
+    }
 
     close(client.socket);
     /*
@@ -141,62 +279,29 @@ while ((n = read(client.socket, buffer, sizeof(buffer) - 1)) > 0) {
     return NULL;
 }
 
+void manageClient(int clients[]);
+
 int main() {
-    int client_socket = 0;int *new_sock = 0;
+    int client_socket = 0;
+    int *new_sock = 0;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    // Gestion des signaux
+    initAdresse(&server_addr);
+    server_socket = initSocket(&server_addr);
+
     signal(SIGINT, sigIntHandler);
     atexit(exitFunction);
 
-    // Initialisation du sémaphore
     sem_init(&mutex, 0, 1);
 
-    // Initialisation des rooms
     for (int i = 0; i < MAX_ROOMS; i++) {
         rooms[i].count = 0;
         rooms[i].history_count = 0;
     }
 
-    // Création du socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        perror("Could not create socket");
-        exit(EXIT_FAILURE);
-    }
-    printf("Socket created...\n");
-
-     // Activer l'option SO_REUSEADDR sur le socket serveur
-    int opt = 1;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
-        perror("Échec de l'activation de SO_REUSEADDR");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    // Préparation de la structure sockaddr_in pour la liaison
-    server_addr.sin_family = AF_INET; // Famille d'adresses IPv4
-    server_addr.sin_addr.s_addr = INADDR_ANY; // Utilisation de toutes les interfaces réseau disponibles
-    server_addr.sin_port = htons(PORT); // Port d'écoute du serveur
-
-    // Liaison du socket
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-    printf("Bind done...\n");
-
-    // Écoute des connexions entrantes
-    if (listen(server_socket, 3) < 0) {
-        perror("Listen failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
     printf("Waiting for incoming connections...\n");
 
-    // Acceptation des connexions entrantes
     while ((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len))) {
         printf("Connection accepted from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
